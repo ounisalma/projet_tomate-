35a57c10b147e245ccec902e74bebef41bd45e37
ounisalma <salmaelouni606@gmail.com>
2025-04-19 10:21:47 +0100

    remove venv from git tracking and ignore it

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predict():
 
 if __name__ == '__main__':
     # Démarrer l'application Flask sur 0.0.0.0 pour accepter les connexions externes
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000)
