23d91c640dd48625a8e9f4dbaa4852270c741076
ounisalma <salmaelouni606@gmail.com>
2025-04-19 12:31:10 +0100

    remove venv from git tracking and ignore it

--- app.py
+++ app.py
@@ -4,23 +4,16 @@ from io import BytesIO
 from flask import Flask, jsonify, request
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-import tensorflow as tf
-import tensorflow_model_optimization as tfmot
 import gc
 
 app = Flask(__name__)
 
 # تحميل النموذج مرة واحدة فقط عند تشغيل التطبيق
-model = None  # النموذج يتم تحميله عند الحاجة فقط
-class_names = ['Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_healthy']
+model = load_model("modele_tomate (1).h5")
+print("LOADED MODEL ✅")
 
-# تحميل النموذج المحسن أو المحول
-def load_model_once():
-    global model
-    if model is None:
-        model = load_model("modele_tomate (1).h5")
-        print("LOADED MODEL ✅")
-    return model
+# أسماء الأصناف حسب ترتيب الدوسيات في التدريب
+class_names = ['Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_healthy']
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -32,13 +25,11 @@ def predict():
         return jsonify({'error': 'No selected file'}), 400
 
     try:
-        # تحميل وتحضير الصورة مع تصغير الحجم
-        img = image.load_img(BytesIO(file.read()), target_size=(128, 128))  # تصغير الحجم إلى 128x128
+        # تحميل وتحضير الصورة
+        img = image.load_img(BytesIO(file.read()), target_size=(224, 224))
         img_array = image.img_to_array(img)
         img_array = img_array / 255.0
         img_array = np.expand_dims(img_array, axis=0)
-
-        model = load_model_once()  # تحميل النموذج فقط عند الحاجة
 
         # التنبؤ بالصنف
         prediction = model.predict(img_array)
